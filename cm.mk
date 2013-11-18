366a29c838c77993abce4311fb949c4098ca73f1
David Hays <dhays90@gmail.com>
2013-11-18 10:32:06 -0600

    t6spr: update fingerprint id

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t6spr BUILD_ID=JZO54K BUILD_FINGERPRINT="htc/t6spr/t6spr:4.3/JSS15J/248380.1:user/release-keys" PRIVATE_BUILD_DESC="1.05.1540.1 CL248380 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=sprint_wwe BUILD_ID=JSS15J BUILD_FINGERPRINT="htc/sprint_wwe/t6whl:4.3/JSS15J/268933.4:user/release-keys" PRIVATE_BUILD_DESC="1.16.651.4 CL268933 release-keys"
 
 PRODUCT_NAME := cm_t6spr
 PRODUCT_DEVICE := t6spr
