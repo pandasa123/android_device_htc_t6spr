4ff4168eabc4be8a15f268fb07cbf068f8777531
David Hays <Dhays90@gmail.com>
2013-11-20 21:07:50 -0600

    t6spr: global mode

--- cm.mk
+++ cm.mk
@@ -1,7 +1,5 @@
 $(call inherit-product, device/htc/t6spr/full_t6spr.mk)
 
-$(call inherit-product, vendor/cm/config/cdma.mk)
-
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
