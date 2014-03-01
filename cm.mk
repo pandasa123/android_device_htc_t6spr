0bc5dcec25692c4264bdbacc9464f3860570ffef
David Hays <Dhays90@gmail.com>
2014-03-01 11:25:44 -0600

    t6spr: Change inherit order
     * Move comon_full_phone.mk to top. Otherwise default notification
       and alarm tone will be completely wrong
    
    Please see the following changes for an explanation/source:
    
    http://review.cyanogenmod.org/#/c/59655/
    http://review.cyanogenmod.org/#/c/59973/

--- cm.mk
+++ cm.mk
@@ -1,8 +1,8 @@
-$(call inherit-product, device/htc/t6spr/full_t6spr.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, device/htc/t6spr/full_t6spr.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=sprint_wwe BUILD_ID=JSS15J BUILD_FINGERPRINT="htc/sprint_wwe/t6whl:4.3/JSS15J/268933.4:user/release-keys" PRIVATE_BUILD_DESC="1.16.651.4 CL268933 release-keys"
 
